8c6c33623cd9683d885dc748e6f9ad7119d5e8bc
imac2 <imac2@imac2s-imac.local>
2018-12-12 09:56:07 +0300

    Initial Commit

---
+++ Denemeurl/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Denemeurl
+//
+//  Created by imac2 on 12/12/18.
+//  Copyright © 2018 imac2. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
